44daa0bba363f727d4e937fb513fe2b81979bd22
BespalovDE <61617903+BespalovDE@users.noreply.github.com>
2020-04-30 23:57:58 +0300

    Retypeset from LW6.1

--- lw8/js/carouselMovie.js
+++ lw8/js/carouselMovie.js
@@ -19,46 +19,46 @@ function getFilmInfo(filmName, stateAdd) {
 function createFilmBefore() {
     const rect = document.createElement('div');
     rect.classList.add('movie');
-    let doc = document.getElementsByClassName('movie_Picture');
+    let doc = document.getElementsByClassName('movie_picture');
     let newData = getFilmInfo(doc[0].getAttribute("alt"), -1);
-    rect.innerHTML = '<img src="' + newData.url + '" class="movie_Picture" alt="' + newData.name + '">'
-                   + '<h4>' + newData.name + '</h4>'
+    rect.innerHTML = '<img src="' + newData.url + '" class="movie_picture" alt="' + newData.name + '">'
+                   + '<h3>' + newData.name + '</h3>'
                    + '<p>' + newData.info + '</p>';
 	return rect;
 }
 
 function createFilmUppend() {
     const rect = document.createElement('div');
-    rect.classList.add('movie', 'left_Movie');
-    let doc = document.getElementsByClassName('movie_Picture');
+    rect.classList.add('movie', 'left_movie');
+    let doc = document.getElementsByClassName('movie_picture');
     let newData = getFilmInfo(doc[doc.length - 1].getAttribute("alt"), 1);
-    rect.innerHTML = '<img src="' + newData.url + '" class="movie_Picture" alt="' + newData.name + '">'
-                   + '<h4>' + newData.name + '</h4>'
+    rect.innerHTML = '<img src="' + newData.url + '" class="movie_picture" alt="' + newData.name + '">'
+                   + '<h3>' + newData.name + '</h3>'
                    + '<p>' + newData.info + '</p>';
 	return rect;
 }
 
 function onAddFilmRight() {
     const newRect = createFilmUppend();
-    let elem = document.getElementById("movie_block");
+    let elem = document.getElementById("movies_block");
     elem.removeChild(elem.firstElementChild);  
-    elem.firstElementChild.classList.remove('left_Movie');
+    elem.firstElementChild.classList.remove('left_movie');
 	elem.appendChild(newRect);
 }
 
 function onAddFilmLeft() {
     const newRect = createFilmBefore();
-    let elem = document.getElementById('movie_block');
+    let elem = document.getElementById('movies_block');
     elem.removeChild(elem.lastElementChild);
-    elem.firstElementChild.classList.add('left_Movie');
+    elem.firstElementChild.classList.add('left_movie');
     elem.prepend(newRect);
 }
 
 
 function run() {
-    const addRectLeftMovie = document.getElementById('rightMovie');
+    const addRectLeftMovie = document.getElementById('right_movie');
     addRectLeftMovie.addEventListener('click', onAddFilmRight); 
-    const addRectRightMovie = document.getElementById('leftMovie');
+    const addRectRightMovie = document.getElementById('left_movie');
     addRectRightMovie.addEventListener('click', onAddFilmLeft);
 }
 
