4f84699f36328f8718673ff93a9ebcf8f3fae2d2
BespalovDE <61617903+BespalovDE@users.noreply.github.com>
2020-05-16 22:23:20 +0300

    Rewrited LW №6 task 2-4

---
+++ lw6/6.2-6.4/src/pages/save_feedback_page.php
@@ -0,0 +1,28 @@
+<?php
+
+function saveFeedbackPage(): void
+{
+    $userData['username'] = getPostParameter('username');
+    $userData['email'] = getPostParameter('email');
+    $userData['from'] = getPostParameter('from');
+    $userData['gender'] = getPostParameter('gender');
+    $userData['message'] = getPostParameter('message');
+    
+    $args = validateUserData($userData);
+    if ($args['isValidate'] === true)
+    {
+        //данные корректны - сохранение анкеты
+        updateProfileFile($userData['email'], $userData);
+        $status = 1;
+        $message = 'Сообщение отправлено!';
+        $valuesBefore = [];
+        $args['errorFields'] = [];
+    }
+    else
+    {
+        $status = 0;
+        $message = 'Заполните корректно поля:';
+        $valuesBefore = $userData;
+    }
+    mainPage(array('status' => $status, 'errorFields' => $args['errorFields'], 'message' => $message, 'valuesBefore' => $valuesBefore));
+}
--- lw6/6.2-6.4/src/templates/main.tpl.php
+++ lw6/6.2-6.4/src/templates/main.tpl.php
@@ -7,28 +7,29 @@
      <link href="css/menustyle.css" rel="stylesheet">
      <link href="css/moviesStyle.css" rel="stylesheet">
      <link href="css/formStyle.css" rel="stylesheet">
+     <link href="css/php_styles.css" rel="stylesheet">
      <title>Anket</title>
   </head>
   <body>
-  <div class="all_Page">
+  <div class="all_page">
     <div class="content">
       <div class="menu_block">
         <ul class="menu_main">
-          <li class = "widthLi_First">
-            <a href="#about">
-              <span class="aboutme_Icon"></span>
+          <li class="menu_main_li width_li_first">
+            <a href="#about" class="menu_main_a">
+              <span class="about_me_icon"></span>
               Обо мне&thinsp;
             </a>
           </li>
-          <li class = "widthLi_Second">
-            <a href="#hobby">
-              <span class="aboutme_Icon heart_Icon"></span>
+          <li class="menu_main_li width_li_second">
+            <a href="#hobby" class="menu_main_a">
+              <span class="about_me_icon heart_icon"></span>
               Моё хобби&thinsp;
             </a>
           </li>
-          <li class = "widthLi_Third">
-            <a href="#films">
-              <span class="aboutme_Icon video_Icon"></span>
+          <li class="menu_main_li width_li_third">
+            <a href="#films" class="menu_main_a">
+              <span class="about_me_icon video_icon"></span>
               Любимые фильмы&thinsp;
             </a>
           </li>
@@ -39,22 +40,22 @@
           <div class="top_block">
              <img src="images/123.jpg" class="photo" alt="My photo">
              <div class="quote">
-               <blockquote>
+               <blockquote class="for_blockquote">
                  Слова ничего не значат, пока не превратишь их в поступки.
-                 <div class="autor_Phrase">—Капитан Шепард. Mass Effect</div>
+                 <div class="autor_phrase">—Капитан Шепард. Mass Effect</div>
                </blockquote>
              </div>
           </div>
-          <div class="fav_Info">
-             <div class="favourite_Head">Любимые писатели:</div>
-             <ul class="spisok">
+          <div class="fav_info">
+             <div class="favourite_head">Любимые писатели:</div>
+             <ul class="list">
               <li>Энн Райс</li>
               <li>Говард Филлипс Лавкрафт</li>
               <li>Братья Струацкие</li>
               <li>Уильям Гибсон</li>
             </ul>
-            <div class="favourite_Head margin_Top42">Любимые фильмы:</div>
-             <ol class="spisok spisok_Next">
+            <div class="favourite_head margin_top_42">Любимые фильмы:</div>
+             <ol class="list list_next">
               <li>Властелин колец</li>
               <li>Матрица</li>
               <li>Призрак оперы</li>
@@ -63,9 +64,9 @@
           </div>
           <div class="info">
             <a name="about"></a>
-            <h1>Беспалов Дмитрий</h1>
-            <div class="line_Block"></div>
-            <p class="spisok margin_Top43 about_Me">
+            <h1 class="for_h1">Беспалов Дмитрий</h1>
+            <div class="line_block"></div>
+            <p class="list margin_top_43 about_me">
               Родился 21.09.1990 года в городе Йошкар-Оле. Учился в средней общей школе №23. Закончил Марийский Государственный Универсистет 
               по специальности "Математические методы в экономике". Далее занимался написанием диссертационной работы в аспирантуре Марийского 
               Государственного Университета по специальности "Математические и инструментальные методы экономики". Закончил аспирантуру, прошёл 
@@ -73,27 +74,27 @@
               так же прохожу обучение в Институте Программных Систем.
             </p>
             <a name="hobby"></a>
-            <h2 class="margin_Top43">Мое хобби</h2>
-            <p class="spisok margin_Top10 about_Me">
+            <h2 class="h2_hobby margin_top_43">Мое хобби</h2>
+            <p class="list margin_top_10 about_me">
               Среди хобби наиболее важное место занимает мотоцикл, книги и электро гитара. Естественно, мотоцикл занимает время только в летний сезон. 
               Среди книг большее предпочтение отдаю таким стилям как научная фантастика, фэнтези, ужасы, мистика. Гитара используется по настроению.
               Бывает что всю неделю можно играть, а бывает на пол года о ней забываю. Так же можно упомянуть малые хобби для вечернего времяпровождения,
               такие как просмотр сериалов, аниме, а так же компьютерные игры. Кроме того, время от времени занимаюсь написанием небольших прорамм просто 
               для общего развития.
             </p>
-            <a href="https://vk.com/dark_advent" class="Arrow">Напиши мне</a>
+            <a href="https://vk.com/dark_advent" class="arrow">Напиши мне</a>
           </div>
         </div>
       </div> 
       <div class="clear_all"></div>
       <div class="movies">
         <a name="films"></a>
-        <h3>Любимые фильмы</h3>
+        <h2 class="h2_movie">Любимые фильмы</h2>
         <div class="movies_block">
           <div class="movie">
-             <img src="images/lor.jpg" class="movie_Picture" alt="Властелин колец">
-             <h4>Властелин колец</h4>
-             <p>
+             <img src="images/lor.jpg" class="movie_picture" alt="Властелин колец">
+             <h3 class="movie_h3">Властелин колец</h3>
+             <p class="movie_p">
                Кинотрилогия «Властелин колец»- снятая режиссёром Питером Джексоном серия из трёх связанных единым сюжетом кинофильмов, представляющая 
                собой экранизацию романа Дж. Р. Р. Толкина «Властелин колец». Сюжет трилогии следует за хоббитом Фродо Бэггинсом, который идёт в поход 
                вместе с Братством Кольца с целью уничтожения Кольца Всевластия. Это необходимо для окончательной победы над его создателем, Тёмным 
@@ -102,10 +103,10 @@
                армиям Саурона в Войне Кольца.
              </p>
           </div>  
-          <div class="movie left_Movie">
-            <img src="images/matrix.jpg" class="movie_Picture" alt="Матрица">
-            <h4>Матрица</h4>
-            <p>
+          <div class="movie left_movie">
+            <img src="images/matrix.jpg" class="movie_picture" alt="Матрица">
+            <h3 class="movie_h3">Матрица</h3>
+            <p class="movie_p">
               «Матрица» — американскo-авcтpалийcкий научно-фантастический боевик, снятый братьями Вачовски. В фильме участвовали Киану Ривз, Лоуренс 
               Фишборн, Керри-Энн Мосс и Хьюго Уивинг. Фильм вышел на экраны США 31 марта 1999 года и положил начало трилогии фильмов. Фильм изображает 
               будущее, в котором реальность, существующая для большинства людей, есть в действительности симуляция типа «мозг в колбе», созданная 
@@ -114,10 +115,10 @@
               также вовлечены другие люди, освободившиеся из «мира снов» и выбравшиеся в реальность.
             </p>
           </div>  
-          <div class="movie left_Movie">
-            <img src="images/phantom.jpg" class="movie_Picture" alt="Призрак оперы">
-            <h4>Призрак оперы</h4>
-            <p>
+          <div class="movie left_movie">
+            <img src="images/phantom.jpg" class="movie_picture" alt="Призрак оперы">
+            <h3 class="movie_h3">Призрак оперы</h3>
+            <p class="movie_p">
               «Призрак Оперы» — американский музыкальный фильм, снятый режиссёром Джоэлом Шумахером в 2004 году по одноимённому мюзиклу Эндрю Ллойда-Уэббера, 
               который в свою очередь поставлен по мотивам одноимённого романа французского писателя Гастона Леру.
               Таинственный голос взывает к молодой оперной певице Кристине из мрачных недр величественной Парижской оперы. Лишь один человек в труппе знает, 
@@ -127,10 +128,10 @@
               возлюбленным свой немилосердный приговор…
             </p>
           </div> 
-          <div class="movie left_Movie">
-            <img src="images/stalker.png" class="movie_Picture" alt="Сталкер">
-            <h4>Сталкер</h4>
-            <p>
+          <div class="movie left_movie">
+            <img src="images/stalker.png" class="movie_picture" alt="Сталкер">
+            <h3 class="movie_h3">Сталкер</h3>
+            <p class="movie_p">
               «Сталкер» — советский фантастический фильм-притча режиссёра Андрея Тарковского, снятый на киностудии «Мосфильм» в 1979 году, по мотивам повести 
               «Пикник на обочине» братьев Стругацких. По сюжету в некой запретной Зоне, по слухам, существует комната, где исполняются самые заветные желания. 
               К этой комнате отправляются модный Писатель и авторитетный Профессор — каждый по своим причинам, о которых предпочитают не говорить. А ведет их 
@@ -138,32 +139,53 @@
             </p>
           </div> 
         </div>
-        <a href="" class="movie_Button">Все фильмы</a>
+        <a href="" class="movie_button">Все фильмы</a>
       </div>
       <div class="clear_all"></div>
-      <div class="form_Block"> 
-        <div class="title">
-          <span>НАПИШИ МНЕ</span>
-        </div>
+      <div class="form_block"> 
+        <div class="title"><h2 class="title_h2">НАПИШИ МНЕ</h2></div>
         <div class="clear"></div>
-
-        <form action="php/saveFile.php" method="POST" > 
-        <? include('php/form.php'); ?>
-          <ul class="menu_main form_Value">
-            <? foreach($fields as $fieldName => $fieldAttrs): ?>
-                <?= $fieldAttrs["username"] ?>
-                <?= $fieldAttrs["email"] ?>
-                <?= $fieldAttrs["choice"] ?>
-                <?= $fieldAttrs["gend"] ?> 
-                <?= $fieldAttrs["mail"] ?> 
-                <?= $fieldAttrs["button"] ?>
-            <? endforeach; ?>
-          </ul>
+        <form class="form_style" method="POST" autocomplete="off">
+          <div class="form_value">
+            <div class="label_first">Ваше имя</div>
+            <input type="text" class="input_field form_margin_between" name="username" value="<?php echo($args['valuesBefore']['username'] ?? ''); ?>" />
+            <div class="label_first">Ваш email</div>
+            <input type="text" class="input_field form_margin_between" name="email" value="<?php echo($args['valuesBefore']['email'] ?? ''); ?>" />
+            <div class="label_second">Откуда вы?</div>
+            <div class="new_select_style form_margin_between"> 
+              <select name="from" class="new_select_style_select">
+                <option class="option_style" value="Не указано" <?php echo printSelected('from', 'Не указано', $args['valuesBefore']); ?> >Не указано</option>
+                <option class="option_style" value="Россия" <?php echo printSelected('from', 'Россия', $args['valuesBefore']); ?> >Россия</option>
+                <option class="option_style" value="Украина" <?php echo printSelected('from', 'Украина', $args['valuesBefore']); ?> >Украина</option>
+                <option class="option_style" value="Беларусь" <?php echo printSelected('from', 'Беларусь', $args['valuesBefore']); ?> >Беларусь</option>
+                <option class="option_style" value="Казахстан" <?php echo printSelected('from', 'Казахстан', $args['valuesBefore']); ?> >Казахстан</option>
+                <option class="option_style" value="Средиземье" <?php echo printSelected('from', 'Средиземье', $args['valuesBefore']); ?> >Средиземье</option>
+                <option class="option_style" value="Нарния" <?php echo printSelected('from', 'Нарния', $args['valuesBefore']); ?> >Нарния</option>
+                <option class="option_style" value="Азерот" <?php echo printSelected('from', 'Азерот', $args['valuesBefore']); ?> >Азерот</option>
+              </select>
+            </div> 
+            <div class="margin_radio"><div class="label_second">Ваш пол</div> 
+            <input type="radio" class="radio_button" name="gender" id="r1" value="Мужской" <?php echo printChecked('gender', 'Мужской', $args['valuesBefore']); ?> /><label class="radio_label" for="r1">Мужской</label>
+            <input type="radio" class="radio_button" name="gender" id="r2" value="Женский" <?php echo printChecked('gender', 'Женский', $args['valuesBefore']); ?> /><label class="radio_label" for="r2">Женский</label> </div>
+            <div class="label_first">Ваше сообщение</div>
+            <textarea class="for_textarea form_margin_between" name="message" wrap="hard"><?php echo $args['valuesBefore']['message'] ?? ''; ?></textarea>
+            <button type="submit" class="form_button" onClick="Handler" value="send">Отправить</button>
+            <span class="form_answer <?php echo isset($args['status']) ? $args['status'] === 1 ? 'success' : 'error' : ''; ?>"><?php echo $args['message']; ?></span>
+              <?php if (isset($args['status']) && $args['status'] === 0) 
+                 { 
+              ?>
+              <ul class="error_fields">
+              <?php foreach($args['errorFields'] as $answer): ?>
+                <li><?php echo $answer; ?></li>
+              <?php endforeach; ?>
+              </ul>
+              <?php } ?>
+          </div>
         </form>
       </div>
     </div>
     <div class="footer">
-      <p>© 2006-2018 Поволжский государственный технологический университет, ФГБОУ ВО «ПГТУ»</p>
+      <p class="footer_p">© 2006-2018 Поволжский государственный технологический университет, ФГБОУ ВО «ПГТУ»</p>
     </div>
   </div>
   </body>
